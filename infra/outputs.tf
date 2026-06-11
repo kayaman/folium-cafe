1765d0baa3eb10ed450752e62cee0f9503ffb819
Marco Antonio Gonzalez Junior <m@rco.sh>
2026-06-11 11:02:06 -0300

    feat(infra): stack outputs for deploy workflow

---
+++ infra/outputs.tf
@@ -0,0 +1,19 @@
+output "site_bucket" {
+  value = aws_s3_bucket.site.bucket
+}
+
+output "distribution_id" {
+  value = aws_cloudfront_distribution.site.id
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.api.function_name
+}
+
+output "url" {
+  value = "https://${var.domain_name}"
+}
+
+output "ci_role_arn" {
+  value = aws_iam_role.ci.arn
+}
